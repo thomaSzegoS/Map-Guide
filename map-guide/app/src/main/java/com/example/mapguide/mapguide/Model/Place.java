1b01410a1d444bb55c8ca735d8794b3f0c1b5f2f
KaterinaStef22 <39234031+KaterinaStef22@users.noreply.github.com>
2018-11-03 19:23:52 +0200

    place class added

--- map-guide/app/src/main/java/com/example/mapguide/mapguide/Model/Place.java
+++ map-guide/app/src/main/java/com/example/mapguide/mapguide/Model/Place.java
@@ -1,5 +1,40 @@
 package com.example.mapguide.mapguide.Model;
 
+import com.google.android.gms.maps.model.LatLng;
+
 public class Place {
+
+
+    private String title, description; // onoma perioxhs ,perigrafh perioxhs
+    private LatLng coordinates; // syntetagmenes latitude ,longtude
+
+    public Place() {
+
+    }
+
+    public Place(String title, String description, LatLng coordinates) {
+        this.title = title;
+        this.description = description;
+        this.coordinates = coordinates;
+    }
+
+    public void setTitle(String title) {
+
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
 
 }
