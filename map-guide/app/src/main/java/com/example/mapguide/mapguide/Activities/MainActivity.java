cbcac353fc8fad848d30e643a677bc238bbe9b48
papacharalampou <44607384+papacharalampou@users.noreply.github.com>
2018-11-06 13:45:32 +0200

    Added FLickr Service

--- map-guide/app/src/main/java/com/example/mapguide/mapguide/Activities/MainActivity.java
+++ map-guide/app/src/main/java/com/example/mapguide/mapguide/Activities/MainActivity.java
@@ -1,20 +1,25 @@
 package com.example.mapguide.mapguide.Activities;
 
-import android.app.Dialog;
-import android.content.Intent;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
 import com.example.mapguide.mapguide.R;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
+import com.example.mapguide.mapguide.Services.FlickrServices;
+
 
 public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        FlickrServices r = new FlickrServices();
+        for (int i = 0; i < r.PhotosData.size(); i++) {
+            String[] Photo = r.PhotosData.get(i);
+            System.out.println("ID:"+Photo[0]+ ", Title:" +Photo[1]+ ", Link:" +Photo[2]+ ", Latitude:" +Photo[3]+ ", Longitude:" +Photo[4]+ ", Description:" +Photo[5]+ " ");
+        }
+
+    }
 
 }
 
