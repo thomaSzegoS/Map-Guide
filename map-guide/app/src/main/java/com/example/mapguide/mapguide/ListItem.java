34b3fe29142b42d61eda1fefb0cd573492db2bbc
Dimitris <jimpal9@gmail.com>
2018-11-14 17:42:53 +0200

    Added most RecyclerView

--- map-guide/app/src/main/java/com/example/mapguide/mapguide/ListItem.java
+++ map-guide/app/src/main/java/com/example/mapguide/mapguide/ListItem.java
@@ -4,10 +4,13 @@ public class ListItem {
 
     private String head;
     private String desc;
+    private String imageUrl;
 
     public ListItem(String head,String desc) {
         this.head = head;
         this.desc = desc;
+        this.imageUrl = imageUrl;
+
     }
 
     public String getHead() {
@@ -16,5 +19,9 @@ public class ListItem {
 
     public String getDesc() {
         return desc;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
     }
 }
