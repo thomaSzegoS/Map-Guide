dc8a8ff9364c5c5ea6f31c9bdb1225711c1e5ff4
KaterinaStef22 <39234031+KaterinaStef22@users.noreply.github.com>
2018-11-20 20:31:04 +0200

    MainActivity

--- map-guide/app/src/main/java/com/example/mapguide/mapguide/Activities/MainActivity.java
+++ map-guide/app/src/main/java/com/example/mapguide/mapguide/Activities/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.mapguide.mapguide.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.mapguide.mapguide.Model.ImageModel;
 import com.example.mapguide.mapguide.R;
 import com.example.mapguide.mapguide.Services.FlickrServices;
 
@@ -15,8 +17,8 @@ public class MainActivity extends AppCompatActivity {
 
         FlickrServices r = new FlickrServices();
         for (int i = 0; i < r.PhotosData.size(); i++) {
-            String[] Photo = r.PhotosData.get(i);
-            System.out.println("ID:"+Photo[0]+ ", Title:" +Photo[1]+ ", Link:" +Photo[2]+ ", Latitude:" +Photo[3]+ ", Longitude:" +Photo[4]+ ", Description:" +Photo[5]+ " ");
+            ImageModel photo = r.PhotosData.get (i);
+            System.out.println ("ID:" + photo.getId () + ", Title:" + photo.getTitle () + ", Link:" + photo.getLinkImg () + ", Latitude:" + photo.getPlace ().getLat () + ", Longitude:" + photo.getPlace ().getLon () + ", Description:" + photo.getDesc () + " ");
         }
 
     }
