6d3d245d6287e1e978302716bbbb279cc9fd8bd8
TsiflikasYourDad1 <37966422+TsiflikasYourDad1@users.noreply.github.com>
2018-12-12 21:43:44 +0200

    Added main_menu bar with some options

---
+++ map-guide/app/src/main/java/com/example/mapguide/mapguide/SettingsActivity.java
@@ -0,0 +1,24 @@
+package com.example.mapguide.mapguide;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class SettingsActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setupActionBar();
+    }
+    /**
+     * Set up the {@link android.app.ActionBar}, if the API is available.
+     */
+    private void setupActionBar() {
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+            // Show the Up button in the action bar.
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+}
