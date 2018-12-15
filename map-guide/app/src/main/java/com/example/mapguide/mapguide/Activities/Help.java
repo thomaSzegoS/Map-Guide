552eb7ecf4c04b4ebf2faa8266eed7e31f8fb42b
TsiflikasYourDad1 <panagioti1997@hotmail.com>
2018-12-15 20:29:57 +0200

    added_help_and_settings_form
    pressing toolbar and shows "settings" and "help".When you click one of them it  navigates you to a new form

---
+++ map-guide/app/src/main/java/com/example/mapguide/mapguide/Activities/Help.java
@@ -0,0 +1,15 @@
+package com.example.mapguide.mapguide.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mapguide.mapguide.R;
+
+public class Help extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help);
+    }
+}
