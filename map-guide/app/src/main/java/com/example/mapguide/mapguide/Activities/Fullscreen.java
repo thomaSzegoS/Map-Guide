ffdd38bec898c0728bced2868f731af611dba195
tasos97p <tac_4444@hotmail.com>
2018-12-19 14:26:04 +0200

    Revert "FullScreen"
    This reverts commit 6745bcfb72a45f145af4b8e76db0834d33f8a104.

--- map-guide/app/src/main/java/com/example/mapguide/mapguide/Activities/Fullscreen.java
+++ map-guide/app/src/main/java/com/example/mapguide/mapguide/Activities/Fullscreen.java
@@ -2,24 +2,13 @@ package com.example.mapguide.mapguide.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
-
 import com.example.mapguide.mapguide.R;
-import com.github.chrisbanes.photoview.PhotoView;
 
 public class Fullscreen extends AppCompatActivity {
-
-    PhotoView fimg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fullscreen);
-        fimg = findViewById(R.id.fullscreenView);
-        fimg.setImageResource(R.drawable.ic_android_black_24dp);
     }
-
-    //private void loadimg(){
-
-   // }
 }
